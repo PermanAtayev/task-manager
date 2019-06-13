13979344328263f506258391ef9c6d6261387cc4
Perman <perman.atayev17@gmail.com>
2019-06-13 17:22:30 +0300

    Some updates

--- src/index.js
+++ src/index.js
@@ -48,6 +48,21 @@ app.get( "/users/:id", async (req, res) => {
     }
 })
 
+app.patch( "/users/:id", async (req, res) =>{
+    try{
+        const user = await User.findByIdAndUpdate( req.params.id, {new:true, setDefaultsOnInsert: true, findAndModify: true, runValidators: true} );
+
+        if( user )
+            return res.status(200).send(user);
+        
+        return res.status(404).send('User was not found');
+    }
+    catch( error ){
+        res.status(400).send(error);
+    }
+
+})
+
 app.post("/tasks", async (req, res) => {
     const task = new Task( req.body );
 
