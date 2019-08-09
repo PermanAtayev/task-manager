24bc0ebe8c0b7d39025fc8df3c4e5a1392fe7767
Perman <perman.atayev17@gmail.com>
2019-08-09 09:30:12 +0300

    Home page is added

--- src/index.js
+++ src/index.js
@@ -12,6 +12,10 @@ app.use(express.json());
 app.use(UserRouter);
 app.use(TaskRouter);
 
+app.get("/", async(req, res) => {
+    res.send("Welcome to the task-manager api, for the documentation how to use it go to: https://github.com/PermanAtayev/task-manager");
+})
+
 app.listen( port, () => {
     console.log("Listening on port -", port);
 });
