e83504aba3b531608ca4fd122c4f38e4a8d74f94
Perman <perman.atayev17@gmail.com>
2019-08-09 09:37:35 +0300

    Main page text

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ const UserRouter = require("./routers/user");
 const TaskRouter = require("./routers/task");
 
 app.use(express.json());
-app.use(UserRouter);
-app.use(TaskRouter);
 
 app.get("/", async(req, res) => {
     res.send("Welcome to the task-manager api, for the documentation how to use it go to: https://github.com/PermanAtayev/task-manager");
 })
+
+app.use(UserRouter);
+app.use(TaskRouter);
+
 
 app.listen( port, () => {
     console.log("Listening on port -", port);
